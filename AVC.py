944138b33490e8337d788c448ea73ee61f935876
mxkopy <mxkopylov@gmail.com>
2020-11-27 04:15:17 -0700

    made network deeper

--- AVC.py
+++ AVC.py
@@ -20,18 +20,17 @@ class ImageEncoder(torch.nn.Module):
 
         super(ImageEncoder, self).__init__()
 
-        # Input is a sizable convolution, hopefully removes noise
-        self.conv1 = torch.nn.Conv2d(3, 4, 5, 2, 1).cuda()
         self.relu1 = torch.nn.ReLU().cuda()
-        self.pool1 = torch.nn.AdaptiveAvgPool2d((256, 256)).cuda()
-
-        self.conv2 = torch.nn.Conv2d(4, 8, 3, 2).cuda()
         self.relu2 = torch.nn.ReLU().cuda()
+
+        self.pool1 = torch.nn.AdaptiveAvgPool2d((512, 512)).cuda()
         self.pool2 = torch.nn.AdaptiveAvgPool2d((64, 64)).cuda()
 
-        self.conv3 = torch.nn.Conv2d(8, 16, 3, 1).cuda()
-        self.relu3 = torch.nn.ReLU().cuda()
-        self.pool3 = torch.nn.AdaptiveAvgPool2d((8, 8)).cuda()
+        self.conv1 = torch.nn.Conv2d(3, 3, 1, 1).cuda()
+        self.conv2 = torch.nn.Conv2d(3, 4, 3, 2).cuda()
+        self.conv3 = torch.nn.Conv2d(4, 8, 3, 1).cuda()
+        self.conv4 = torch.nn.Conv2d(8, 12, 1, 1).cuda()
+        self.conv5 = torch.nn.Conv2d(12, 16, 3, 1).cuda()
 
         self.out = torch.nn.ReLU().cuda()
 
@@ -39,16 +38,17 @@ class ImageEncoder(torch.nn.Module):
     def forward(self, x):
 
         out = self.conv1(x)
-        # out = self.relu1(out)
         out = self.pool1(out)
 
+        out = self.relu1(out)
         out = self.conv2(out)
-        # out = self.relu2(out)
         out = self.pool2(out)
 
         out = self.conv3(out)
-        # out = self.relu3(out)
-        out = self.pool3(out)
+        out = self.conv4(out)
+
+        out = self.relu2(out)
+        out = self.conv5(out)
 
         return out
 
@@ -60,34 +60,25 @@ class ImageDecoder(torch.nn.Module):
 
         super(ImageDecoder, self).__init__()
 
-        self.deconv1 = torch.nn.ConvTranspose2d(16, 8, 8, 3).cuda()
-        self.relu1 = torch.nn.ReLU().cuda()
-        self.pool1 = torch.nn.AdaptiveAvgPool2d((8, 8)).cuda()
+        self.pool = torch.nn.AdaptiveAvgPool2d((512, 512)).cuda()
 
-
-        self.deconv2 = torch.nn.ConvTranspose2d(8, 4, 5, 2).cuda()
-        self.relu2 = torch.nn.ReLU().cuda()
-        self.pool2 = torch.nn.AdaptiveAvgPool2d((64, 64)).cuda()
-
-        self.deconv3 = torch.nn.ConvTranspose2d(4, 3, 3, 1).cuda()
-        self.relu3 = torch.nn.ReLU().cuda()
-        self.pool3 = torch.nn.AdaptiveAvgPool2d((512, 512)).cuda()
+        self.deconv1 = torch.nn.ConvTranspose2d(16, 12, 3, 1).cuda()
+        self.deconv2 = torch.nn.ConvTranspose2d(12, 8, 1, 1).cuda()
+        self.deconv3 = torch.nn.ConvTranspose2d(8, 4, 3, 1).cuda()
+        self.deconv4 = torch.nn.ConvTranspose2d(4, 3, 3, 2).cuda()
+        self.deconv5 = torch.nn.ConvTranspose2d(3, 3, 1, 1).cuda()
 
 
 
     def forward(self, x):
 
         out = self.deconv1(x)
-        # out = self.relu1(out)
-        out = self.pool1(out)
-
         out = self.deconv2(out)
-        # out = self.relu2(out)
-        out = self.pool2(out)
-
         out = self.deconv3(out)
-        # out = self.relu3(out)
-        out = self.pool3(out)
+        out = self.deconv4(out)
+        out = self.deconv5(out)
+
+        out = self.pool(out)
 
         return out
 
@@ -99,14 +90,16 @@ class AudioEncoder(torch.nn.Module):
         
         super(AudioEncoder, self).__init__()
 
-        self.conv1 = torch.nn.Conv1d(2, 4, 5, 2).cuda()
+        self.conv1 = torch.nn.Conv1d(2, 2, 1, 1).cuda()
         self.relu1 = torch.nn.ReLU().cuda()
 
-        self.conv2 = torch.nn.Conv1d(4, 8, 3, 1).cuda()
+        self.conv2 = torch.nn.Conv1d(2, 4, 3, 2).cuda()
         self.relu2 = torch.nn.ReLU().cuda()
 
-        self.conv3 = torch.nn.Conv1d(8, 16, 3, 1).cuda()
+        self.conv3 = torch.nn.Conv1d(4, 16, 3, 1).cuda()
         self.relu3 = torch.nn.ReLU().cuda()
+
+        self.conv4= torch.nn.Conv1d(4, 16, 3, 1).cuda()
 
         self.pool1 = torch.nn.AdaptiveAvgPool1d(64)
 
@@ -136,6 +129,6 @@ class AudioDecoder(torch.nn.Module):
         self.deconv2 = torch.nn.ConvTranspose1d(8, 4, 1, 3).cuda()
         self.relu2 = torch.nn.ReLU().cuda()
 
-        self.deconv3 = torch.nn.ConvTranspose1d(4, 3, 1, 3).cuda()
+        self.deconv3 = torch.nn.ConvTranspose1d(4, 3, 1, 1).cuda()
         self.relu3 = torch.nn.ReLU().cuda()
 
--- main.py
+++ main.py
@@ -22,29 +22,25 @@ decoder = ImageDecoder().to(device)
 audio_encoder = AudioEncoder()
 audio_decoder = AudioDecoder()
 
+encoder.train()
+decoder.train()
+
 to_pil = torchvision.transforms.ToPILImage()
 
 loss = torch.nn.MSELoss()
 lr = 1e-2
 optimizer = Adam(list(encoder.parameters()) + list(decoder.parameters()), lr)
 
-model_path = os.getcwd() + '/models'
+project_path = os.getcwd() + '/AudioVisualCorrelator'
 
-
-# dynamic, might wanna find a way to crawl through /video
-video_path = os.getcwd() + '/video/video.mp4'
-
-dataset = VideoDataset(video_path)
-data = DataLoader(dataset, batch_size=4)
+model_path = project_path + '/models'
 
 # find a way to plug these values in
 resize = torchvision.transforms.Resize((512, 512))
 
-encoder.train()
-
-decoder.train()
 
 num_encoder_params = 0
+num_decoder_params = 0
 
 for i in encoder.parameters():
     num_encoder_params += 1
@@ -55,25 +51,13 @@ for i in decoder.parameters():
     num_decoder_params += 1
 
 
-
-
-
-
-
-def visualize_parameters(k):
+def visualize_parameters():
     
     encoder_params = encoder.parameters()
-    decoder_params = decoder.parameters() 
 
-    img = np.zeros((3, (num_encoder_params  + num_decoder_params) * k, k * k))
+    params = encoder_params[0]
 
-    for i in range(num_encoder_params  // k):
-
-        img[1 : 3, i * k : (i + 1) * k, : ] = np.average(encoder_params[i].detach().numpy())
-
-    for i in range(num_decoder_params // k):
-
-        img[0 : 2, i * k : (i + 1) * k, : ] = np.average(decoder_params[i].detach().numpy())
+    print(params.shape())
 
     return img
 
@@ -81,36 +65,31 @@ def visualize_parameters(k):
 
 def train(clean_up_index=100):
 
-    encoder = ImageEncoder()
-    decdoer = ImageDecoder()
+    for path in os.listdir(project_path + '/video'):
 
-    for path in os.listdir('./video/'):
-
-        dataset = VideoDataset(os.getcwd() + '/video/' + path)
+        dataset = VideoDataset(project_path + '/video/' + path)
         data = DataLoader(dataset)
 
         for epoch in range(4):
 
             running_loss = 0.0
+            
+            clean_index = 0
 
             for video, audio in data:
 
                 optimizer.zero_grad()
-
                 video = video.to('cuda')
 
                 img_out = encoder(video)
-
                 img_out = decoder(img_out)
 
                 truth = resize(video)
 
                 curr_loss = loss(img_out, truth)
-
                 running_loss += curr_loss.item()
 
                 curr_loss.backward()
-
                 optimizer.step()
 
                 print(running_loss)
@@ -125,15 +104,16 @@ def train(clean_up_index=100):
 
                 cv.waitKey(1)
 
-            if i > clean_up_index:
+            if clean_index > clean_up_index:
                 cv.destroyAllWindows() 
+                torch.cuda.empty_cache()
+
+            clean_index += 1
 
             torch.save({
                 'encoder' : encoder.state_dict(),
                 'decoder' : decoder.state_dict(),
-                'optimizer' : optimizer.state_dict() },'./models/model.pt')
-
-
+                'optimizer' : optimizer.state_dict() }, project_path + '/models/model.pt')
 
 
 
@@ -146,6 +126,6 @@ def test():
 
 
 
-train()
+# train()
 
 #test()
